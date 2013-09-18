cc49914379eeceee456a94a2cd9ed000c367be2e
Robert Dober <robert.dober@gmail.com>
2013-09-18 18:38:19 +0200

    fixes #3 and #4

--- spec/dir/files_spec.rb
+++ spec/dir/files_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe Dir do 
   context "#files" do
-    it "implements stub_dir_with_content" do
+    before do
       stub_dir_with_content "hello", "a", "b/c", "b/"
-      Dir.files( "hello" ).to_a.sort.should eq([["a", File.join(ENV["PWD"], "a")], ["b/c", File.join(ENV["PWD"],"b/c")]])
+    end
+
+    let :expected do
+      [["a", File.join(ENV["PWD"], "a")], ["b/c", File.join(ENV["PWD"],"b/c")]]
+    end
+    it "implements stub_dir_with_content" do
+      Dir.files( "hello" ).to_a.sort.should eq(expected)
+    end
+    it "takes a block" do
+      x = []
+      Dir.files( "hello" ){ |fn,pn| x << [fn, pn] }
+      x.should eq( expected )
     end
   end
 end # describe Dir
