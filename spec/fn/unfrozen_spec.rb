ff1ad327a0f5cd891ab0527f090d9f157c81677c
Robert Dober <robert.dober@gmail.com>
2013-10-03 19:43:05 +0200

    routine bup. nxt: implement more iterators

--- spec/fn/unfrozen_spec.rb
+++ spec/fn/unfrozen_spec.rb
@@ -8,20 +8,4 @@ describe Object do
   it "can use fn" do
     expect( Integer.fn.cksum.(12) ).to eq( 4 )
   end
-
-  it "can be used as a block for arrays" do
-    expect( [*source].map Integer.fn.cksum ).to eq( [9,1,2,3] )
-  end
-
-  it "can be used as a block for enumerables" do
-    expect( source.map Integer.fn.cksum ).to eq( [9,1,2,3] )
-  end
-
-  it "can be used for enumerators" do
-    expect( source.to_enum.map( Integer.fn.cksum ).to_a ).to eq( [9,1,2,3] )
-  end
-
-  it "can be used for lazy enumerators" do
-    expect( source.lazy.map( Integer.fn.cksum ).to_a ).to eq( [9,1,2,3] )
-  end
 end # describe Object
