9bb3d7f627d6c865a4dca85662a5605f2ab74bea
Robert Dober <robert.dober@gmail.com>
2013-10-03 19:24:10 +0200

    basic specs for #7 pass

--- spec/fn/unfrozen_spec.rb
+++ spec/fn/unfrozen_spec.rb
@@ -1,22 +1,27 @@
 require 'spec_helper'
 require 'lab42/core/fn'
 
-describe Object, :wip do
+describe Object do
+  let(:source){8..11}
+  let(:expected){[9,1,2,3]}
+
   it "can use fn" do
     expect( Integer.fn.cksum.(12) ).to eq( 4 )
   end
 
   it "can be used as a block for arrays" do
-    expect( [*8..11].map Integer.fn.cksum ).to eq( [9,1,2,3] )
+    expect( [*source].map Integer.fn.cksum ).to eq( [9,1,2,3] )
   end
+
   it "can be used as a block for enumerables" do
-    expect( (8..11).map Integer.fn.cksum ).to eq( [9,1,2,3] )
+    expect( source.map Integer.fn.cksum ).to eq( [9,1,2,3] )
   end
 
   it "can be used for enumerators" do
-    expect( (8..11).to_enum.map( Integer.fn.cksum ).to_a ).to eq( [9,1,2,3] )
+    expect( source.to_enum.map( Integer.fn.cksum ).to_a ).to eq( [9,1,2,3] )
   end
+
   it "can be used for lazy enumerators" do
-    expect( (8..11).lazy.map( Integer.fn.cksum ).to_a ).to eq( [9,1,2,3] )
+    expect( source.lazy.map( Integer.fn.cksum ).to_a ).to eq( [9,1,2,3] )
   end
 end # describe Object
