b7c138dca09150c3dcf34e3069ae3b5d00a7a87b
Robert Dober <robert.dober@gmail.com>
2013-10-03 22:07:34 +0200

    ready for 0.0.4

--- lib/lab42/core/fn/iterator_reimpl.rb
+++ lib/lab42/core/fn/iterator_reimpl.rb
@@ -7,6 +7,23 @@ module Lab42
           def map behavior=nil, &blk
             __lab42_core_iterator_map__(&(behavior||blk))
           end
+          alias_method :__lab42_core_iterator_reduce__, :reduce
+          def reduce behavior=nil, &blk
+            if Symbol === behavior
+              __lab42_core_iterator_reduce__ behavior
+            else
+              __lab42_core_iterator_reduce__(&(behavior||blk))
+            end
+          end
+
+          alias_method :__lab42_core_iterator_inject__, :inject
+          def inject value, behavior=nil, &blk
+            if Symbol === behavior
+              __lab42_core_iterator_inject__ value, behavior
+            else
+              __lab42_core_iterator_inject__(value, &(behavior||blk))
+            end
+          end
         end
       end
     end
