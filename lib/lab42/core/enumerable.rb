01573fabc8b09b51d83500ca428005e3146c6f35
rd <rd@aegle.fr>
2014-03-25 11:14:33 +0100

    working on enumerable take_while

--- lib/lab42/core/enumerable.rb
+++ lib/lab42/core/enumerable.rb
@@ -2,4 +2,6 @@ module Enumerable
   def grep2 expr
     partition{ |ele| expr === ele }
   end
+
+  def take_while 
 end
---
+++ spec/enumerable/take_while_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Enumerable do 
+  
+  context "take_while with accumulator (ary)" do 
+    subject do
+      %w{alpha beta gamma delta epsilon}
+    end
+
+    it 'passes the accumulator' do
+      expect( subject.take_while { |_,acc|
+                                   acc.join(' ').size < 11
+      }).to eq( %w{alpha beta} )
+    end
+  end # context "take_while with accumulator"
+  
+  
+  context "take_while with accumulator (enum)" do 
+    subject do
+      1..100
+    end
+
+    # REMARK:
+    # Would it not be lovely to write the expression below as:
+    #    subject.inject_while(0, :+, sendmsg(:<, 100))
+    it 'passes the accumulator' do
+      expect( subject.take_while { |_,acc|
+                                   acc.reduce( :+ ) < 100
+      }).to eq( %w{alpha beta} )
+    end
+  end # context "take_while with accumulator"
+  
+end # describe Enumerable
